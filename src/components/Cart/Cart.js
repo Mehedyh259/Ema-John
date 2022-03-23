16e83ba13f23461f26d7c645c27966e9ddead5ac
Mehedyh259 <mehedyh259@gmail.com>
2022-03-23 20:25:52 +0600

    first commit

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Cart.css';
+const Cart = ({ cart }) => {
+    console.log(cart);
+    let total = 0;
+    let shipping = 0;
+    let quantity = 0;
+    for (const product of cart) {
+        quantity += product.quantity;
+        total += (product.price * product.quantity);
+        shipping += product.shipping;
+    }
+    const tax = total * 0.1;
+    const grandTotal = total + shipping + tax;
+    return (
+        <div className='cart'>
+            <h2>Order Summery</h2>
+            <p>Selected Items: {quantity}</p>
+            <p>Total Price: ${total}</p>
+            <p>Total Shipping: ${shipping}</p>
+            <p>Tax: ${tax.toFixed(2)}</p>
+            <h5>Grand Total: ${grandTotal}</h5>
+        </div>
+    );
+};
+
+export default Cart;
---
+++ src/components/Product/Product.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import './Product.css';
+const Product = (props) => {
+    const { handleAddToCart, product } = props;
+    const { name, img, seller, price, ratings } = product;
+
+    return (
+        <div className='product'>
+            <img src={img} alt="" />
+            <div className="product-info">
+                <p className='product-name'>{name}</p>
+                <p>Price: ${price}</p>
+                <p><small>Seller: {seller}</small></p>
+                <p><small>Rating: {ratings} stars</small></p>
+            </div>
+            <button onClick={() => handleAddToCart(product)} className="button-cart">Add to Cart <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon></button>
+        </div>
+    );
+};
+
+export default Product; 
